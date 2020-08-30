37328a03fc75549eb093c93e33e6ecb71380bfa7
Elijah Johnson <57976922+elijahrj1@users.noreply.github.com>
2020-08-30 15:18:38 -0400

    Add files via upload

---
+++ cse1322l/module1/Assignment1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Text;
+
+namespace assignment_1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var cypherObject = new Cypher(); 
+            cypherObject.cypherMethod();
+            cypherObject.reverseMethod();
+            Console.Write(cypherObject.ToString());
+            Console.WriteLine();
+            Console.WriteLine("Input a phrase to be scrambled.");
+            string original = Console.ReadLine();
+            var testCypherObject = new Cypher(original);
+            testCypherObject.cypherMethod();
+            testCypherObject.reverseMethod();
+            Console.WriteLine(testCypherObject.ToString());
+        }
+    }
+
+    public class Cypher
+    {
+        private string original;
+        private string encrypted;
+        private string decrypted;
+
+        public Cypher()
+        {
+            original = "cypher";
+        }
+
+        public Cypher(string input)
+        {
+            original = input;
+        }
+
+        public void cypherMethod()
+        {
+            var evens = new StringBuilder();
+            var odds = new StringBuilder();
+
+            for (int i = 0; i < original.Length; i++)
+            {
+                if ((i + 1) % 2 == 1)
+                {
+                    odds.Append(original[i]);
+                }
+                else
+                {
+                    evens.Append(original[i]);
+                }
+            }
+            encrypted = odds.ToString() + evens.ToString();
+        }
+
+        public void reverseMethod()
+        {
+            var output = new StringBuilder(encrypted.Length);
+            var odds = encrypted.Substring(0, encrypted.Length / 2);
+            var evens = encrypted.Substring(encrypted.Length / 2);
+
+            for (int i = 0, oi = -1, oe = -1; i < encrypted.Length; i++)
+            {
+                if ((i + 1) % 2 == 1)
+                {
+                    output.Append(odds[++oi]);
+                }
+
+                else
+                {
+                    output.Append(evens[++oe]);
+                }
+            }
+
+            decrypted = output.ToString();
+        }
+
+        public string GetOriginal()
+        {
+            return original;
+        }
+
+        public string GetEncrypted()
+        {
+            return encrypted;
+        }
+
+        public string GetDecrypted()
+        {
+            return decrypted;
+        }
+
+        public void SetOriginal(string val)
+        {
+            original = val;
+        }
+
+        public void SetEncrypted(string val)
+        {
+            encrypted = val;
+        }
+
+        public void SetDecrypted(string val)
+        {
+            decrypted = val;
+        }
+
+        public override string ToString()
+        {
+            return "Original Message: " + original + "\nEncrypted: " + encrypted + "\nDecrypted: " + decrypted;
+        }
+
+    }
+}
---
+++ cse1322l/module1/Lab1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace lab_1
+{
+    public class Lab1
+    {
+        public static void Main(string[] args)
+        {
+            int i, j, scale = 9;
+            int x = 1, z = 1;
+
+            // pattern B
+            Console.WriteLine("Pattern B");
+            for (i = 0; i < scale; i++)
+            {
+                for (j = 0; j < scale; j++)
+                {
+                    if (i + j < scale - 1)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.Write("* ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            // pattern C
+            Console.WriteLine();
+            Console.WriteLine("Pattern C");
+            for (i = 0; i < scale; i++)
+            {
+                for (j = 0; j < scale; j++)
+                {
+                    if (i <= j)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("- ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            // pattern D
+            Console.WriteLine();
+            Console.WriteLine("Pattern D");
+            for (i = 0; i >= 0; i += x)
+            {
+                for (j = 0; j >= 0; j = j + z)
+                {
+                    if (i + j >= scale / 2)
+                    {
+                        Console.Write("* ");
+                    }
+                    else if (z == 1)
+                    {
+                        Console.Write("- ");
+                    }
+
+                    if (j >= scale / 2)
+                    {
+                        z = -1;
+                    }
+                }
+                Console.WriteLine();
+                if (i >= scale / 2)
+                {
+                    x = -1;
+                }
+            }
+        }
+    }
+}
