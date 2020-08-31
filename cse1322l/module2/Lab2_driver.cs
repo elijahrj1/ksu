afb4f98da75f8f3aa191b79a2901c14aedadd6be
Elijah Johnson <57976922+elijahrj1@users.noreply.github.com>
2020-08-30 20:16:14 -0400

    Add files via upload

---
+++ cse1322l/module2/Lab2_driver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab2
+{
+    class StockItemDriver
+    {
+        static void Main(string[] args)
+        {
+            StockItem stock = new StockItem();
+            Console.Write("Enter Description: ");
+            stock.SetDescription(Console.ReadLine());
+            Console.Write("Enter Amount: ");
+            stock.AddAmount(Convert.ToInt32(Console.ReadLine()));
+            Console.Write("Enter Price: ");
+            stock.SetPrice(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine(stock);
+            Console.Write("Enter Amount To Remove: ");
+            stock.RemoveAmount(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("Final: \n" + stock);
+        }
+    }
+}
